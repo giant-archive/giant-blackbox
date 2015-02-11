6b7011dc23e20415f6e20f447f5b8c7ec2f4e4d5
Leon Smith <_@leonmarksmith.com>
2015-02-11 14:21:21 +0000

    Fix url tests

--- src/tests.py
+++ src/tests.py
@@ -1,15 +1,11 @@
-import binascii
-import json
-import time
 import os
-import requests
 import re
-
-from BeautifulSoup import BeautifulSoup
-from selenium import webdriver
-
 import unittest
-from unittest.util import safe_repr
+import unittest.util
+from datetime import datetime
+
+import requests_cache
+from BeautifulSoup import BeautifulSoup
 
 
 class BlackBoxTestCase(unittest.TestCase):
@@ -26,11 +22,9 @@ class BlackBoxTestCase(unittest.TestCase):
         super(BlackBoxTestCase, self).setUp()
 
         # Get the domain from the environment variable.
-        domain = os.getenv("BAMBOO_DOMAIN", '')
+        domain = os.getenv("BAMBOO_DOMAIN")
 
-        # Make sure it contains http://
-        if "http://" not in domain:
-            domain = "http://%s" % domain
+        assert "http://" in domain or 'https://' in domain
 
         # Make we remove any trailing slash.
         if domain.endswith("/"):
@@ -38,11 +32,12 @@ class BlackBoxTestCase(unittest.TestCase):
 
         # We're done with the domain manipulation.
         self.domain = domain
+        self.domain_404 = "%s/does-not-exist-%s" % (self.domain, datetime.now())
 
         # Setup the requests session.
-        self.session = requests.Session()
+        self.session = requests_cache.CachedSession()
+        self.session.timeout = 5
         self.session.headers.update({'User-Agent': 'FARM Digital Black Box Test Agent 1.0'})
-
 
     def tearDown(self):
         """
@@ -50,41 +45,33 @@ class BlackBoxTestCase(unittest.TestCase):
         really crappy way to prevent overwhelming a site.
         """
 
-        time.sleep(2)
-
-
     def assertResponseIsOk(self, response, msg=None):
         self.assertTrue(response.ok, msg=msg)
 
-
     def assertResponseIsNotFound(self, response, msg=None):
         self.assertTrue(response.status_code, 404)
-
 
     def assertResponseRedirectsTo(self, response, expected, msg=None):
         self.assertTrue(response.history, 'No redirection')
         self._baseAssertEqual(response.url, expected, msg=msg)
 
-
     def assertResponseMaxRedirects(self, response, maximum, msg=None):
-        standard_msg = 'Exceeded maximum number of redirects (%s/%s)' \
-                       % (safe_repr(len(response.history)), safe_repr(maximum))
+        standard_msg = 'Exceeded maximum number of redirects (%s/%s)' % (
+            unittest.util.safe_repr(len(response.history)),
+            unittest.util.safe_repr(maximum)
+        )
         msg = self._formatMessage(msg, standard_msg)
         self.assertLessEqual(maximum, len(response.history), msg=msg)
 
-
     def assertResponseHeadersContains(self, response, header, msg=None):
         self.assertIn(header, response.headers, msg=msg)
-
 
     def assertResponseHeaderEquals(self, response, header, value, msg=None):
         self.assertResponseHeadersContains(response, header, msg=msg)
         self.assertEqual(response.headers.get(header, ''), value, msg=msg)
 
-
     def assertResponseContains(self, response, text, msg=None):
         self.assertIn(text, response.text, msg=msg)
-
 
     def assertResponseDoesNotContain(self, response, text, msg=None):
         self.assertNotIn(text, response.text, msg=msg)
@@ -97,11 +84,28 @@ class TestRedirects(BlackBoxTestCase):
 
     def test_canonical_redirect(self):
 
-        source = self.domain.replace("www.", '') if "www." in self.domain else "www.%s" % self.domain
-        response = self.session.get(source)
-        self.assertResponseIsOk(response)
-        self.assertResponseRedirectsTo(response, self.domain)
-        self.assertResponseMaxRedirects(response, 1)
+        root_domain = self.domain.replace('http://', '').replace('https://', '').replace('www.', '')
+        https = 'https' in self.domain
+        urls = []
+
+        # Build a list of urls to check
+        for protocol in ['https://', 'http://']:
+            urls += ["%s%s" % (protocol, root_domain), "%swww.%s" % (protocol, root_domain)]
+
+        print urls
+        # Pop our final domain
+        urls.remove(self.domain)
+
+        # Remove https is we are not running on https
+        urls = [url for url in urls if 'https://' in url and not https]
+
+        print urls
+
+        for url in urls:
+            response = self.session.get(url)
+            self.assertResponseIsOk(response)
+            self.assertResponseRedirectsTo(response, self.domain)
+            self.assertResponseMaxRedirects(response, 1)
 
 
 class TestStaticFiles(BlackBoxTestCase):
@@ -114,7 +118,6 @@ class TestStaticFiles(BlackBoxTestCase):
         self.assertResponseIsOk(response)
         self.assertResponseHeadersContains(response, 'content-type', 'text/plain')
 
-
     def test_robots_txt_disallow(self):
         """
         Test that we haven't left a redundant "Disallow: /" in robots.txt.
@@ -122,7 +125,6 @@ class TestStaticFiles(BlackBoxTestCase):
         response = self.session.get('%s/robots.txt' % self.domain)
         self.assertResponseIsOk(response)
         self.assertResponseDoesNotContain(response, "Disallow: /\n")
-
 
     def test_favicon(self):
         """
@@ -147,60 +149,6 @@ class TestStaticFiles(BlackBoxTestCase):
         # Get the favicon
         icon_response = self.session.get(icon_url)
         self.assertResponseIsOk(icon_response)
-
-
-class TestOnPage(BlackBoxTestCase):
-    """
-    This uses the Selenium WebDriver to evaluate the page and allow us to inspect it.
-    """
-
-    def test_screenshot(self):
-        """
-        This takes a PNG screenshot of a give site. This test should always pass (unless Phantom is very broken).
-        """
-
-        # Setup the PhantomJS session.
-        self.phantom = webdriver.PhantomJS("/opt/phantomjs/bin/phantomjs")
-
-        # Grab the page.
-        self.phantom.get(self.domain)
-
-        # Save the screenshot.
-        self.phantom.set_window_size(1024, 768)
-
-        # Pause for a few seconds, then snap the screenshow.
-        time.sleep(5)
-        self.phantom.save_screenshot("screenshot.png")
-
-        # Pass the test.
-        self.assertEqual(True, True)
-
-    def test_page_resources(self):
-        """
-        This test will fail if any resources loaded by the index page source returns anything
-        but a 200 response.
-        """
-
-        # Setup the PhantomJS session.
-        self.phantom = webdriver.PhantomJS("/opt/phantomjs/bin/phantomjs")
-
-        # Grab the page.
-        self.phantom.get(self.domain)
-
-        # Grab the HAR log.
-        raw_log = self.phantom.get_log('har')
-
-        # Parse the log
-        log = json.loads(raw_log[0]['message'])
-
-        # Find any of the responses where the status code isn't 200.
-        failing_requests = filter(lambda x: 200 != x['response']['status'], log['log']['entries'])
-
-        # Fail the test if any page requests have failed.
-        self.assertEquals(len(failing_requests), 0)
-
-        # Tidy up.
-        self.phantom.quit()
 
 
 class TestSitemap(BlackBoxTestCase):
@@ -236,12 +184,10 @@ class TestHeaders(BlackBoxTestCase):
     This tests various response headers.
     """
 
-    def setUp(self):
-        super(TestHeaders, self).setUp()
-
     def test_gzip_enabled(self):
-        response = self.session.get(self.domain)
-        self.assertResponseHeaderEquals(response, 'content-encoding', 'gzip')
+        if os.getenv("BALCKBOX_TEST_GZIP", False):
+            response = self.session.get(self.domain)
+            self.assertResponseHeaderEquals(response, 'content-encoding', 'gzip')
 
 
 class TestResponseCodes(BlackBoxTestCase):
@@ -255,8 +201,7 @@ class TestResponseCodes(BlackBoxTestCase):
         self.assertResponseHeadersContains(response, 'content-type', 'text/html')
 
     def test_404(self):
-        random_junk = binascii.b2a_hex(os.urandom(15))
-        response = self.session.get("%s/%s" % (self.domain, random_junk))
+        response = self.session.get(self.domain_404)
         self.assertResponseIsNotFound(response)
 
 
@@ -270,7 +215,5 @@ class TestDjangoConfig(BlackBoxTestCase):
         Here we test that DEBUG isn't enabled, by looking for the string
         "Django" in the 404 response. This might be naive, but it works.
         """
-
-        random_junk = binascii.b2a_hex(os.urandom(15))
-        response = self.session.get("%s/%s" % (self.domain, random_junk))
+        response = self.session.get(self.domain_404)
         self.assertResponseDoesNotContain(response, "Django")
